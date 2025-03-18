42e205734cf4568fefca7810a64e610165e63d02
GBarbosa <andradegui2006@gmail.com>
2025-03-18 14:12:55 -0300

    Update ValidationMessages.jsx

--- login-page/src/components/ValidationMessages.jsx
+++ login-page/src/components/ValidationMessages.jsx
@@ -1,17 +1,30 @@
+import { Check, X } from "lucide-react";
 const PasswordValidationMessages = ({ nameFieldUsed, emailFieldUsed, samePass, sixChars }) => {
     return (
       <div>
         <p className={`${nameFieldUsed ? "text-green-400" : "text-red-400"} font-semibold text-sm h-4`}>
-          {nameFieldUsed ? "Campo nome usado" : "Campo nome vazio"}
+          {nameFieldUsed ? 
+            <p className="flex gap-1 align-middle"><Check size={16} /> <span className="-translate-y-0.5">Campo nome usado</span></p> 
+            : 
+            <p className="flex gap-1 align-middle"><X size={16} /><span className="-translate-y-0.5">Campo nome vazio</span></p>}
         </p>
         <p className={`${emailFieldUsed ? "text-green-400" : "text-red-400"} font-semibold text-sm h-4`}>
-          {emailFieldUsed ? "Campo de email usado" : "Campo de email vazio"}
+          {emailFieldUsed ? 
+            <p className="flex gap-1 align-middle"><Check size={16} /><span className="-translate-y-0.5">Campo de email usado</span></p> 
+            :
+            <p className="flex gap-1 align-middle"><X size={16} /><span className="-translate-y-0.5">Campo de email vazio</span></p>}
         </p>
         <p className={`${samePass ? "text-green-400" : "text-red-400"} font-semibold text-sm h-4`}>
-          {samePass ? "As senhas são iguais" : "As senhas são diferentes"}
+          {samePass ? 
+            <p className="flex gap-1 align-middle"><Check size={16} /><span className="-translate-y-0.5">As senhas são iguais</span></p> 
+            :
+            <p className="flex gap-1 align-middle"><X size={16} /><span className="-translate-y-0.5">As senhas são diferentes</span></p>}
         </p>
         <p className={`${sixChars ? "text-green-400" : "text-red-400"} font-semibold text-sm h-4`}>
-          {sixChars ? "A senha tem 6 ou mais caracteres" : "A senha precisa ter pelo menos 6 caracteres"}
+          {sixChars ? 
+            <p className="flex gap-1 align-middle"><Check size={16} /><span className="-translate-y-0.5">A senha tem 6 ou mais caracteres</span></p> 
+            :
+            <p className="flex gap-1 align-middle"><X size={16} /><span className="-translate-y-0.5">A senha precisa ter pelo menos 6 caracteres</span></p>}
         </p>
       </div>
     );
