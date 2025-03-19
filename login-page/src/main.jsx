6f83818794fc97c9a6006e2ecafb9b6f99f4d0af
GBarbosa <andradegui2006@gmail.com>
2025-03-19 12:21:42 -0300

    Update main.jsx

--- login-page/src/main.jsx
+++ login-page/src/main.jsx
@@ -5,8 +5,20 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
+  // ! Após a criação de ambas as paginas (login e register), criar uma inicial, que direcionará para ou login ou register, usando router
+  // ? avaliar mudança de cor do sistema
+  // ? avaliar criação de modo claro e escuro 
+  /* { 
+    path: "/",
+    element: "<App />"
+  }, */
   {
-    path: "/login"
+    path: "/login",
+    element: "<App />"
+  },
+  {
+    path: "/register",
+    element: "<App />"
   }
 ])
 
