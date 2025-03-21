69927ea6b167945463ebd0e77111956e2722135a
GBarbosa <andradegui2006@gmail.com>
2025-03-21 19:48:45 -0300

    Background beams

--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -4,8 +4,9 @@ import Input from "./components/Input";
 import { Eye, EyeOff } from "lucide-react";
 import PageChange from "./components/PageChange";
 import { useNavigate } from "react-router-dom";
+import { BackgroundBeams } from "./components/ui/background-beams";
 
-function Login() {
+function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [show, setShow] = useState(false);
@@ -30,53 +31,56 @@ function Login() {
   }
 
   return (
-    <div className="w-screen h-screen bg-gradient-to-r from-slate-800 to-slate-950 flex justify-center items-center flex-col text-white">
-      <Title>Login</Title>
-      <form className="flex flex-col gap-2">
-        <Input
-          placeholder="Email"
-          value={email}
-          type="text"
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-        <div className="flex flex-row gap-2">
+    <div className="w-screen h-screen bg-gradient-to-r from-slate-800 to-slate-950 flex justify-center items-center flex-col text-white relative">
+      <BackgroundBeams className="absolute top-0 left-0 z-0" />
+      <div className="flex flex-col items-center relative z-10">
+        <Title>Login</Title>
+        <form className="flex flex-col gap-2" onSubmit={login}>
           <Input
-            placeholder="Senha"
-            value={password}
-            type={show ? "text" : "password"}
+            placeholder="Email"
+            value={email}
+            type="text"
             onChange={(event) => {
-              setPassword(event.target.value);
+              setEmail(event.target.value);
             }}
           />
-          <span
-            className="flex self-center cursor-pointer select-none"
-            onClick={() => setShow(!show)}
+          <div className="flex flex-row gap-2">
+            <Input
+              placeholder="Senha"
+              value={password}
+              type={show ? "text" : "password"}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <span
+              className="flex self-center cursor-pointer select-none"
+              onClick={() => setShow(!show)}
+            >
+              {show ? <Eye size={33} /> : <EyeOff size={33} />}
+            </span>
+          </div>
+          <button
+            type="submit"
+            className={`rounded-md w-90 h-13 transition-all
+                          ${
+                            !emailFieldUsed || !passFieldUsed
+                              ? "bg-gray-700 cursor-not-allowed"
+                              : "bg-gradient-to-r from-purple-500 to-pink-500 font-extrabold p-3 hover:brightness-110 cursor-pointer"
+                          }`}
+            disabled={!emailFieldUsed || !passFieldUsed}
+            
           >
-            {show ? <Eye size={33} /> : <EyeOff size={33} />}
-          </span>
-        </div>
-        <button
-          type="submit"
-          className={`rounded-md w-90 h-13 transition-all
-                        ${
-                          !emailFieldUsed || !passFieldUsed
-                            ? "bg-gray-700 cursor-not-allowed"
-                            : "bg-gradient-to-r from-purple-500 to-pink-500 font-extrabold p-3 hover:brightness-110 cursor-pointer"
-                        }`}
-          disabled={!emailFieldUsed || !passFieldUsed}
-          onClick={login}
-        >
-          Cadastrar
-        </button>
-      </form>
-
-      <PageChange>Ainda não tem uma conta? <button className="text-blue-300 cursor-pointer hover:text-blue-400" onClick={() => navigate("/register")}>Registrar</button></PageChange>
-      <PageChange><button className="text-blue-300 cursor-pointer hover:text-blue-400" onClick={() => alert("Um e-mail com instruções para redefinir sua senha foi enviado. Verifique sua caixa de entrada e spam")}>Esqueci minha senha</button> </PageChange>
-
+            Cadastrar
+          </button>
+        </form>
+        <PageChange>Ainda não tem uma conta? <button className="text-blue-300 cursor-pointer hover:text-blue-400" onClick={() => navigate("/register")}>Registrar</button></PageChange>
+        <PageChange><button className="text-blue-300 cursor-pointer hover:text-blue-400" onClick={() => alert("Um e-mail com instruções para redefinir sua senha foi enviado. Verifique sua caixa de entrada e spam")}>Esqueci minha senha</button> </PageChange>
+        
+      </div>
+      
     </div>
   );
 }
 
-export default Login;
+export default App;
--- login-page/src/pages/Register.jsx
+++ login-page/src/pages/Register.jsx
@@ -5,6 +5,7 @@ import ValidationMessages from "../components/ValidationMessages";
 import { Eye, EyeOff } from "lucide-react";
 import PageChange from "../components/PageChange";
 import { useNavigate } from "react-router-dom";
+import {BackgroundBeams} from "../components/ui/background-beams";
 
 function Register() {
   const [name, setName] = useState("");
@@ -32,80 +33,83 @@ function Register() {
     event.preventDefault();
 
     alert("Cadastro realizado com sucesso!");
+    setName("");
+    setEmail("");
+    setPassword("");
+    setConfirm("")
   }
 
   return (
     <div className="w-screen h-screen bg-gradient-to-r from-slate-800 to-slate-950 flex justify-center items-center flex-col text-white">
-      <Title>Cadastro</Title>
-      <form className="flex flex-col gap-2">
-        <Input
-          placeholder="Nome"
-          value={name}
-          type="text"
-          onChange={(event) => {
-            setName(event.target.value);
-          }}
-        />
-
-        <Input
-          placeholder="E-mail"
-          value={email}
-          type="email"
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
-
-        <div className="flex flex-row">
-          <div className="flex flex-col gap-2 flex-2 mr-4">
-            <Input
-              placeholder="Senha"
-              type={show ? "text" : "password"}
-              value={password}
-              onChange={(event) => {
-                setPassword(event.target.value);
-              }}
-            />
-
-            <Input
-              placeholder="Confirme sua senha"
-              type={show ? "text" : "password"}
-              value={confirm}
-              onChange={(event) => {
-                setConfirm(event.target.value);
-              }}
-            />
+      <BackgroundBeams className="z-0" />
+        <div className="flex flex-col items-center z-10">
+        <Title>Cadastro</Title>
+        <form className="flex flex-col gap-2">
+          <Input
+            placeholder="Nome"
+            value={name}
+            type="text"
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <Input
+            placeholder="E-mail"
+            value={email}
+            type="email"
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+          <div className="flex flex-row">
+            <div className="flex flex-col gap-2 flex-2 mr-4">
+              <Input
+                placeholder="Senha"
+                type={show ? "text" : "password"}
+                value={password}
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+              <Input
+                placeholder="Confirme sua senha"
+                type={show ? "text" : "password"}
+                value={confirm}
+                onChange={(event) => {
+                  setConfirm(event.target.value);
+                }}
+              />
+            </div>
+            <span
+              className="flex self-center cursor-pointer select-none"
+              onClick={() => setShow(!show)}
+            >
+              {show ? <Eye size={50} /> : <EyeOff size={50} />}
+            </span>
           </div>
-          <span
-            className="flex self-center cursor-pointer select-none"
-            onClick={() => setShow(!show)}
+          <ValidationMessages
+            nameFieldUsed={nameFieldUsed}
+            emailFieldUsed={emailFieldUsed}
+            samePass={samePass}
+            sixChars={sixChars}
+          />
+          <button
+            type="submit"
+            className={`rounded-md w-90 h-15 transition-all
+                          ${
+                            !nameFieldUsed || !emailFieldUsed || !samePass || !sixChars
+                              ? "bg-gray-700 cursor-not-allowed"
+                              : "bg-gradient-to-r from-purple-500 to-pink-500 font-extrabold p-3 hover:brightness-110 cursor-pointer"
+                          }`}
+            onClick={register}
+            disabled={!nameFieldUsed || !emailFieldUsed || !samePass || !sixChars}
           >
-            {show ? <Eye size={50} /> : <EyeOff size={50} />}
-          </span>
-        </div>
-        <ValidationMessages
-          nameFieldUsed={nameFieldUsed}
-          emailFieldUsed={emailFieldUsed}
-          samePass={samePass}
-          sixChars={sixChars}
-        />
-        <button
-          type="submit"
-          className={`rounded-md w-90 h-15 transition-all
-                        ${
-                          !samePass || !sixChars
-                            ? "bg-gray-700 cursor-not-allowed"
-                            : "bg-gradient-to-r from-purple-500 to-pink-500 font-extrabold p-3 hover:brightness-110 cursor-pointer"
-                        }`}
-          onClick={register}
-          disabled={!nameFieldUsed || !emailFieldUsed || !samePass || !sixChars}
-        >
-          Cadastrar
-        </button>
-      </form>
-
-      <PageChange>Já tem um conta? <button className="text-blue-300 cursor-pointer hover:text-blue-400" onClick={() => navigate("/")}>Faça login</button></PageChange>
-    
+            Cadastrar
+          </button>
+        </form>
+        <PageChange>Já tem uma conta? <button className="text-blue-300 cursor-pointer hover:text-blue-400" onClick={() => navigate("/")}>Faça login</button></PageChange>
+      
+      </div>
     </div>
   );
 }
